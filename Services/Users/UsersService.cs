2defc201d4c5415bd2d43e7fdb70f43c3e851179
Allorak <allorakspam@gmail.com>
2021-12-26 13:03:21 +0300

    Added fake courses and tasks at registration

--- Services/Users/UsersService.cs
+++ Services/Users/UsersService.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
@@ -28,8 +29,14 @@ namespace patools.Services.Users
             var user = _mapper.Map<User>(newUser);
             user.ID = Guid.NewGuid();
             await _context.Users.AddAsync(user);
-            await _context.SaveChangesAsync();
 
+            if (user.Role == UserRoles.Teacher)
+                await CreateFakeTeacherConnections(user);
+
+            if (user.Role == UserRoles.Student)
+                await CreateFakeStudentConnections(user);
+            
+            await _context.SaveChangesAsync();
             return new SuccessfulResponse<GetNewUserDtoResponse>(_mapper.Map<GetNewUserDtoResponse>(user));
         }
 
@@ -47,6 +54,133 @@ namespace patools.Services.Users
             using var hmac = new System.Security.Cryptography.HMACSHA512();
             passwordSalt = hmac.Key;
             passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+        }
+        
+        //TODO: Удалить на релизе
+        private async Task CreateFakeTeacherConnections(User user)
+        {
+            Console.WriteLine("Создаем курсы");
+            List<Course> courses = new List<Course>();
+            for (int i = 0; i < 3; i++)
+            {
+                courses.Add(
+                    new Course()
+                    {
+                        ID = Guid.NewGuid(),
+                        Title = "Курс для " + user.Fullname + " #" + (i + 1),
+                        Teacher = user,
+                        Subject = "Предмет #"+(i+1)
+                    }
+                );
+            }
+            
+            List<PeeringTask> tasks = new List<PeeringTask>();
+            for (int i = 0; i < 8; i++)
+            {
+                tasks.Add(
+                    new PeeringTask()
+                    {
+                        ID = Guid.NewGuid(),
+                        Title = "Задание для "+user.Fullname+" #"+(i+1),
+                        Course = courses[new Random().Next(3)],
+                        SubmissionStartDateTime = DateTime.Now.AddDays(new Random().Next(7)),
+                        SubmissionEndDateTime = DateTime.Now.AddDays(new Random().Next(7,14)),
+                        ReviewStartDateTime = DateTime.Now.AddDays(new Random().Next(14,21)),
+                        ReviewEndDateTime = DateTime.Now.AddDays(new Random().Next(21,28)),
+                        SubmissionsToCheck = new Random().Next(5)+1
+                    });
+            }
+
+            List<Question> questions = new List<Question>();
+            foreach (var task in tasks)
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    questions.Add(
+                        new Question()
+                        {
+                            ID = Guid.NewGuid(),
+                            Order = i,
+                            Title = "Вопрос сдачи #"+(i+1),
+                            Required = new Random().Next(2) != 0,
+                            Type = QuestionTypes.Text,
+                            RespondentType = RespondentTypes.Author,
+                            PeeringTask = task
+                        });
+                    
+                    questions.Add(
+                        new Question()
+                        {
+                            ID = Guid.NewGuid(),
+                            Order = i,
+                            Title = "Вопрос проверки #"+(i+1),
+                            Required = new Random().Next(2) != 0,
+                            Type = QuestionTypes.Text,
+                            RespondentType = RespondentTypes.Peer,
+                            PeeringTask = task
+                        });
+                }
+            }
+
+            await _context.Courses.AddRangeAsync(courses);
+            await _context.Tasks.AddRangeAsync(tasks);
+            await _context.Questions.AddRangeAsync(questions);
+        }
+
+        private async Task CreateFakeStudentConnections(User user)
+        {
+            
+            var courses = await _context.Courses.Select(c => c).ToListAsync();
+
+            var firstCourse = courses[new Random().Next(courses.Count / 2)];
+            var secondCourse = courses[new Random().Next(courses.Count / 2 + 1, courses.Count)];
+
+            var firstCourseConnection = new CourseUser()
+            {
+                ID = Guid.NewGuid(),
+                Course = firstCourse,
+                User = user
+            };
+            var secondCourseConnection = new CourseUser()
+            {
+                ID = Guid.NewGuid(),
+                Course = secondCourse,
+                User = user
+            };
+
+            await _context.CourseUsers.AddAsync(firstCourseConnection);
+            await _context.CourseUsers.AddAsync(secondCourseConnection);
+
+            var firstTasks = await _context.Tasks.Where(t => t.Course == firstCourse).ToListAsync();
+            var secondTasks = await _context.Tasks.Where(t => t.Course == secondCourse).ToListAsync();
+
+            var firstTasksUser = new List<PeeringTaskUser>();
+            var secondTasksUser = new List<PeeringTaskUser>();
+
+            for (int i = 0; i < firstTasks.Count; i++)
+            {
+                firstTasksUser.Add(new PeeringTaskUser()
+                {
+                    ID = Guid.NewGuid(),
+                    PeeringTask = firstTasks[i],
+                    State = PeeringTaskState.Assigned,
+                    Student = user
+                });
+            }
+            
+            for (int i = 0; i < secondTasks.Count; i++)
+            {
+                secondTasksUser.Add(new PeeringTaskUser()
+                {
+                    ID = Guid.NewGuid(),
+                    PeeringTask = secondTasks[i],
+                    State = PeeringTaskState.Assigned,
+                    Student = user
+                });
+            }
+
+            await _context.TaskUsers.AddRangeAsync(firstTasksUser);
+            await _context.TaskUsers.AddRangeAsync(secondTasksUser);
         }
     }
 }
