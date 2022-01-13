7d6f895cbad4911149d3c7a38e46948c0df56424
Мухаммад Юсупов <mayusupov@miem.hse.ru>
2022-01-14 01:47:29 +0300

    FIX UI Get Experts

--- ClientApp/src/api/getExperts/getExperts.ts
+++ ClientApp/src/api/getExperts/getExperts.ts
@@ -1,7 +1,6 @@
 import { AxiosRequestConfig } from 'axios'
 import { IRequestData, IResponseData } from '.'
 import { api, IResponse } from '..'
-import { mock } from './mock'
 
 
 export const getExperts = async ({
@@ -9,22 +8,14 @@ export const getExperts = async ({
   taskId
 }: IRequestData): Promise<IResponse<IResponseData>> => {
 
-  const isMock = true
-
   const requestConfig: AxiosRequestConfig = {
     method: 'GET',
-    url: `/api/v1/tasks/${taskId}/experts`,
+    url: `/api/v1/experts/get/task=${taskId}`,
     headers: {
       'Authorization': `Bearer ${accessToken}`,
       'Accept-Language': 'ru',
     }
   }
-
-  if (isMock) {
-    const response = await mock(requestConfig)
-    return response
-  }
-
 
   const response = await api.request<IResponse<IResponseData>>(requestConfig)
   return response.data
